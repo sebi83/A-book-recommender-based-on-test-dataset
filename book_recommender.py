3e167768cfc4d7133a89efc30ab4cbef0c339737
S3b1aN <Sebiansachs@protonmail.com>
2023-03-07 18:07:00 +0100

    Search engine modification

--- book_recommender.py
+++ book_recommender.py
@@ -1,73 +1,105 @@
+from fuzzywuzzy import fuzz
 import streamlit as st
 import pandas as pd
 import numpy as np
-from fuzzywuzzy import fuzz,process
 
-# Load dataset
-@st.cache_data
-def load_data():
-    ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='cp1251', sep=';', low_memory=False, on_bad_lines='skip')
-    ratings = ratings[ratings['Book-Rating'] != 0]
-    books = pd.read_csv('BX-Books.csv', encoding='cp1251', sep=';', low_memory=False, on_bad_lines='skip')
-    dataset = pd.merge(ratings, books, on=['ISBN'])
-    dataset.dropna(inplace=True)
-    return dataset
+# Load data
+ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='cp1251', sep=';', low_memory=False, on_bad_lines='skip')
+books = pd.read_csv('BX-Books.csv', encoding='cp1251', sep=';', on_bad_lines='skip', low_memory=False)
 
-dataset = load_data()
+# lowercase book titles and authors
+dataset = pd.merge(ratings, books, on=['ISBN'])
+dataset = dataset.apply(lambda x: x.str.lower() if (x.dtype == 'object') else x)
 
-# Get user input
-st.title('Book Recommendation Engine')
-book_input = st.text_input('Enter the name of a book:', 'The Fellowship of the Ring')
+# Function to generate book recommendations based on input book
+def find_closest_match(book_title):
+    book_titles = dataset['Book-Title'].unique()
+    matches = []
+    for title in book_titles:
+        ratio = fuzz.ratio(book_title, title.lower())
+        if ratio >= 70:
+            matches.append(title)
+    return matches
 
-# Prepare dataset for recommendation
-dataset_lowercase = dataset.apply(lambda x: x.str.lower() if x.dtype == 'object' else x)
-readers = dataset_lowercase['User-ID'][dataset_lowercase['Book-Title']==book_input.lower()]
-readers = readers.tolist()
-readers = np.unique(readers)
-
-books_of_readers = dataset_lowercase[(dataset_lowercase['User-ID'].isin(readers))]
-number_of_rating_per_book = books_of_readers.groupby(['Book-Title']).agg('count').reset_index()
-books_to_compare = number_of_rating_per_book['Book-Title'][number_of_rating_per_book['User-ID'] >= 8]
-books_to_compare = books_to_compare.tolist()
-
-ratings_data_raw = books_of_readers[['User-ID', 'Book-Rating', 'Book-Title']][books_of_readers['Book-Title'].isin(books_to_compare)]
-ratings_data_raw_nodup = ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Book-Rating'].mean()
-ratings_data_raw_nodup = ratings_data_raw_nodup.to_frame().reset_index()
-dataset_for_corr = ratings_data_raw_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
-
-# Compute recommendations
-def get_recommendations(dataset, book_title):
+# Function to generate book recommendations based on input book
+def generate_recommendations(book_title):
+    # Check if input book exists in the dataset
     book_title = book_title.lower()
     if book_title not in dataset['Book-Title'].str.lower().unique():
-        close_matches = []
-        for title in dataset['Book-Title'].str.lower().unique():
-            ratio = fuzz.ratio(book_title, title)
-            if ratio >= 70:
-                close_matches.append((title, ratio))
-        if len(close_matches) > 0:
-            close_matches = sorted(close_matches, key=lambda x: x[1], reverse=True)
-            closest_match = close_matches[0][0]
-            st.write(f"Book title not found in dataset. Did you mean '{closest_match}'?")
-            book_ratings = dataset[closest_match]
-            similar_books = dataset.corrwith(book_ratings)
-            similar_books = similar_books.dropna().sort_values(ascending=False)
-            return similar_books.head(10)
+        matches = find_closest_match(book_title)
+        if len(matches) > 0:
+            book_title = matches[0]
+            st.warning(f"Book not found in the dataset. Did you mean {book_title}?")
         else:
-            st.write("Book title not found in dataset.")
-            return None
-    else:
-        book_ratings = dataset[book_title]
-        similar_books = dataset.corrwith(book_ratings)
-        similar_books = similar_books.dropna().sort_values(ascending=False)
-        return similar_books.head(10)
+            st.warning("Book not found in the dataset.")
+            return []
+    
+    # Get readers who read input book
+    readers = dataset.loc[dataset['Book-Title'].str.lower() == book_title, 'User-ID'].unique()
 
-# Compute recommendations for user input
-similar_books = get_recommendations(dataset_for_corr, book_input)
+    # Check if there are any readers who read the input book
+    if len(readers) == 0:
+        st.warning("Book not found in the dataset.")
+        return []
 
-if similar_books is not None:
-    st.write("Books similar to " + book_input + ":")
-    for title, score in similar_books.items():
-        st.write("- " + title.capitalize()) 
-        
-else:
-    st.write('Try another book title.')
+
+    # Get data for readers who read input book
+    books_readers = dataset.loc[dataset['User-ID'].isin(readers)]
+
+    # Number of ratings per book
+    book_ratings = books_readers.groupby(['Book-Title']).agg('count').reset_index()
+
+    # Select books with high enough number of ratings
+    books_to_compare = book_ratings.loc[book_ratings['User-ID'] >= 8, 'Book-Title'].tolist()
+
+    # Get rating data for selected books
+    ratings_data_raw = books_readers.loc[books_readers['Book-Title'].isin(books_to_compare), ['User-ID', 'Book-Rating', 'Book-Title']]
+
+    # Compute mean ratings for each book
+    ratings_data_nodup = ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Book-Rating'].mean()
+    ratings_data_nodup = ratings_data_nodup.to_frame().reset_index()
+
+    # Create correlation matrix for books
+    book_matrix = ratings_data_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
+
+    # Compute correlation between input book and other books
+    book_corr = book_matrix.corrwith(book_matrix[book_title])
+
+    # Combine correlation and average rating into dataframe
+    corr_df = pd.DataFrame({'Correlation': book_corr, 'Avg Rating': book_matrix.mean()}, index=book_matrix.columns)
+
+    # Filter out books with too few ratings
+    corr_df = corr_df.dropna()
+    corr_df = corr_df[corr_df['Avg Rating'] > 8]
+
+    # Sort by correlation and average rating
+    corr_df = corr_df.sort_values(['Correlation', 'Avg Rating'], ascending=[False, False])
+
+    # Return top 10 recommendations
+    return corr_df.sort_values(['Correlation', 'Avg Rating'], ascending=[False, False]).head(10).index.tolist()
+
+
+# Streamlit app
+with st.spinner("Loading data..."):
+    st.title("Book Recommendation System")
+
+book_input = st.text_input("Enter your favorite book:", "")
+
+if st.button("Generate Recommendations"):
+    # Show loading spinner
+    with st.spinner("Generating recommendations..."):
+        # Find closest match to input book title
+        closest_matches = find_closest_match(book_input)
+        if len(closest_matches) > 0:
+            st.warning(f"Book not found in the dataset. Did you mean one of these? {', '.join(closest_matches)}")
+        else:
+            # Generate recommendations
+            recommendations = generate_recommendations(book_input)
+
+            # Display recommendations
+            st.subheader("Top 10 Recommended Books:")
+            if len(recommendations) == 0:
+                st.warning("No recommendations found.")
+            else:
+                for i, book in enumerate(recommendations):
+                    st.write(f"{i+1}. {book}")
---
+++ book_recommender2.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# load data
+@st.cache_data
+def load_data():
+    ratings = pd.read_csv('BX-Book-Ratings.csv', encoding='cp1251', sep=';',on_bad_lines='skip', low_memory=False)
+    ratings = ratings[ratings['Book-Rating']!=0]
+    books = pd.read_csv('BX-Books.csv',  encoding='cp1251', sep=';',on_bad_lines='skip', low_memory=False)
+    dataset = pd.merge(ratings, books, on=['ISBN'])
+    dataset_lowercase = dataset.apply(lambda x: x.str.lower() if(x.dtype == 'object') else x)
+    return dataset_lowercase
+
+# function to get top 10 recommendations
+def get_recommendations(dataset, book_title):
+    # Get all readers who read a book with a similar title and author
+    book_readers_all = dataset['User-ID'][(dataset['Book-Title']==book_title) & (dataset['Book-Author']]
+    book_readers_all = book_readers_all.tolist()
+    book_readers_all = np.unique(book_readers_all)
+    
+    # Get all books read by these readers
+    books_of_book_readers_all = dataset[(dataset['User-ID'].isin(book_readers_all))]
+    
+    # Filter books with enough ratings
+    number_of_rating_per_book = books_of_book_readers_all.groupby(['Book-Title']).agg('count').reset_index()
+    books_to_compare = number_of_rating_per_book['Book-Title'][number_of_rating_per_book['User-ID'] >= 8]
+    books_to_compare = books_to_compare.tolist()
+    
+    # Get ratings of books to compare
+    ratings_data_raw = books_of_book_readers_all[['User-ID', 'Book-Rating', 'Book-Title']][books_of_book_readers_all['Book-Title'].isin(books_to_compare)]
+    ratings_data_raw_nodup = ratings_data_raw.groupby(['User-ID', 'Book-Title'])['Book-Rating'].mean()
+    ratings_data_raw_nodup = ratings_data_raw_nodup.to_frame().reset_index()
+    
+    # Convert ratings data to a pivot table for computing correlations
+    dataset_for_corr = ratings_data_raw_nodup.pivot(index='User-ID', columns='Book-Title', values='Book-Rating')
+    
+    # Compute correlations between books
+    if book_title in dataset_for_corr.columns:  # check if book_title is in dataset_for_corr
+        correlations = dataset_for_corr.corrwith(dataset_for_corr[book_title])
+        corr_df = pd.DataFrame(correlations, columns=['Correlation'])
+        corr_df.dropna(inplace=True)
+        corr_df = corr_df.join(ratings_data_raw_nodup.set_index('Book-Title'), on='Book-Title')
+        corr_df.drop_duplicates(subset=['User-ID'], inplace=True)
+        recommendations = corr_df.groupby('Book-Title').apply(lambda x: x.sort_values(by='Correlation', ascending=False).head(10))
+        recommendations.reset_index(inplace=True, drop=True)
+        return recommendations['Book-Title'].tolist()
+    else:
+        return None  # return None if book_title is not in dataset_for_corr
+
+# create Streamlit app
+def main():
+    st.title("Book Recommendation App")
+    data = load_data()
+    book_title = st.text_input("Enter book title:", "")
+    if st.button("Get Recommendations"):
+        book_title = book_title.strip().lower()
+        if len(book_title) == 0:
+            st.write("Please enter a book title")
+        else:
+            recommendations = get_recommendations(data, book_title)
+            if recommendations is not None:
+                st.write("Top 10 recommendations for ", book_title)
+                for i, book in enumerate(recommendations):
+                    st.write(i+1, book)
+            else:
+                st.write("Sorry, we could not find any recommendations for ", book_title)
+                
+                
+if __name__ == "__main__":
+    main()
