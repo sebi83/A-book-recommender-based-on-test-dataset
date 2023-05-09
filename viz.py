c4bad247845d3bb2d83839af23a87794b878a369
sebi83 <sebastiansachs@protonmail.com>
2023-05-09 15:05:29 +0200

    updated readme.md

--- viz.py
+++ viz.py
@@ -9,7 +9,7 @@ ratings = ratings[ratings['Book-Rating'] != 0]
 #  books DF
 books = pd.read_csv('BX-Books.csv', encoding='cp1251', sep=';', error_bad_lines=False,low_memory=False)
 
-# Outliers
+# Outliers box plot
 plt.figure(figsize=(10, 6))
 plt.boxplot(ratings['Book-Rating'], vert=False)
 plt.xlabel('Book-Rating')
